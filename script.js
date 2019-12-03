b62cc7d38230e70043b5eb2aa8dff69ae48a9c18
Joss <carloscorrea.dev@gmail.com>
2019-12-03 17:35:32 -0600

    initial game + assets

---
+++ script.js
@@ -0,0 +1,25 @@
+// Recursos disponibles ✨
+// Personajes
+const player1 = "images/Character-1.png";
+const player2 = "images/Character-2.png";
+const player3 = "images/Character-3.png";
+const player4 = "images/Character-4.png";
+// Enemigos
+const enemy1 = "images/Enemy-1.png";
+const enemy2 = "images/Enemy-2.png";
+const enemy3 = "images/Enemy-3.png";
+// Minions
+const minion1 = "images/Minion-1.png";
+const minion2 = "images/Minion-2.png";
+const minion3 = "images/Minion-3.png";
+// Armas
+const weapon1 = "images/w-1.png";
+const weapon2 = "images/w-2.png";
+const weapon3 = "images/w-3.png";
+const weapon4 = "images/w-4.png";
+const weapon5 = "images/w-5.png";
+// Fondo
+const bg = "images/bg.jpg";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
